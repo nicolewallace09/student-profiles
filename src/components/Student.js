627fc6a82cc2156602e0e9aa7cf0064d9d69bad1
nicolewallace09 <nicole.elisaw@gmail.com>
2021-06-04 21:29:12 -0700

    styling of tags

--- src/components/Student.js
+++ src/components/Student.js
@@ -39,7 +39,7 @@ function Student(props) {
     const [tags, setTags] = useState([]);
 
     const addTags = event => {
-		if (event.key === 'Enter') {
+		if (event.key === 'Enter' && event.target.value !== '') {
 			setTags([...tags, event.target.value]);
 			event.target.value = "";
 		}
@@ -69,25 +69,25 @@ function Student(props) {
 
 
                         <div className="tags-section">
-                        <ul id="tags">
-                        { tags.map((tag, index) => (
-                        <li key={index} className="tag">
-                        <span className='tag-name'>{tag}</span>
-                        <span className='close' onClick={() => removeTags(index)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-x" viewBox="0 0 16 16">
-                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                            </svg>
-                        </span>
-                        </li>
-                        ))}
-                        </ul>
+                            <ul id="tags">
+                                {tags.map((tag, index) => (
+                                    <li key={index} className="tag">
+                                        <span className='tag-name'>{tag}</span>
+                                        <span className='close' onClick={() => removeTags(index)}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-x" viewBox="0 0 16 16">
+                                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                            </svg>
+                                        </span>
+                                    </li>
+                                ))}
+                            </ul>
 
-                        <input
-                        className="tags-input"
-                        type="text"
-                        onKeyUp={event => event.key === "Enter" ? addTags(event) : null}
-                        placeholder="Add tag"
-                        />
+                            <input
+                            className="tags-input"
+                            type="text"
+                            onKeyUp={event => event.key === "Enter" ? addTags(event) : null}
+                            placeholder="Add tag"
+                            />
                         </div>
 
 
